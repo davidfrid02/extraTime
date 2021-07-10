81bd81cdedf1f1b3b4c434842933f1a37ecbaaac
davidfrid02 <frid.david@gmail.com>
2021-07-10 19:58:44 +0300

    third commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 app.get('/', (request, response) => {
-	response.send('Hello World!')
+	response.send('Welcome to the biggest DATA of the Israel football league');
+})
+
+app.get('/champion', (request, response) => {
+	response.send('beitar Jerusalem!!!');
 })
 
 module.exports = app;
